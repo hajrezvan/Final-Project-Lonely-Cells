4fbfbaab6522c8121ac6911208c61c646c193063
Haj-Rezvan <58388933+hajrezvan@users.noreply.github.com>
2020-01-20 22:27:38 +0330

    Update table
    Table edited

--- Final Project/table.c
+++ Final Project/table.c
@@ -3,44 +3,51 @@
 #include "table.h"
 #include "menus.h"
 
-#define SIZE_X map_x
+#define SIZE_X 9
 #define ROW_BLOCK_SIZE 4
 #define COL_BLOCK_SIZE 8
 
+// Use the SIZE_X for initialize visual_map
 char visual_map[4*SIZE_X+1][8*SIZE_X+1];
 
 void draw_table(void){
     int i;
-    for(i = 0; i <= ROW_BLOCK_SIZE * SIZE_X; i++)
+    for(i = 0; i <= ROW_BLOCK_SIZE * map_y; i++)
     {
         int j;
-        for(j = 0;j <= COL_BLOCK_SIZE * SIZE_X; j++)
+        int k = 0;
+        for(j = 0;j <= COL_BLOCK_SIZE * map_x; j++)
         {
-            if(j % COL_BLOCK_SIZE == 0 && i % ROW_BLOCK_SIZE == 0)
-                visual_map[i][j] = '+';
-
-            else if(i % ROW_BLOCK_SIZE == 0)
-            {
-                if(j % 2 == 0)
-                    visual_map[i][j] = '*';
-                else
+            if (k % 8 == 0 && k != 16){
+                visual_map[i][j] = '|';
+            } else if (k < 8){
+                if (i % 4 == 0){
+                    visual_map[i][j] = '-';
+                } else{
                     visual_map[i][j] = ' ';
+                }
+            } else if (k == 8) {
+                visual_map[i][j] = '|';
+            } else if (k < 16){
+                if ((i + 2) % 4 == 0){
+                    visual_map[i][j] = '-';
+                } else{
+                    visual_map[i][j] = ' ';
+                }
+            } else if (k == 16){
+                visual_map[i][j] = '|';
+                k = 0;
             }
-            else if(j%COL_BLOCK_SIZE == 0)
-            {
-                visual_map[i][j] = '*';
-            }
-
-            else
-                visual_map[i][j] = ' ';
+            k++;
         }
     }
+    print();
 }
 
 void print(){
     int i,j;
-    for (i = 0; i <= ROW_BLOCK_SIZE * SIZE_X; i++){
-        for (j = 0; j <= COL_BLOCK_SIZE * SIZE_X; j++) {
+    for (i = 0; i <= ROW_BLOCK_SIZE * map_y; i++){
+        for (j = 0; j <= COL_BLOCK_SIZE * map_x; j++) {
             printf("%c", visual_map[i][j]);
         }
         printf("\n");
